c6ed62eaef21ecbf7df3a8fa77764790a27544be
GMTurbo <gmturbo@me.com>
2014-10-18 16:36:58 -0600

    remove caching for performance

--- test/text.js
+++ test/text.js
@@ -4,7 +4,7 @@ You can load the Stream base classes by doing require('stream'). There are base 
 
 This document is split up into 3 sections. The first explains the parts of the API that you need to be aware of to use streams in your programs. If you never implement a streaming API yourself, you can stop there.
 
-The second section explainn the parts of the API that you need to use if you implement your own custom streams yourself. The API is designed to make this easy for you to do.
+The second section explains the parts of the API that you need to use if you implement your own custom streams yourself. The API is designed to make this easy for you to do.
 
 The third section goes into more depth about how streams work, including some of the internal mechanisms and functions that you should probably not modify unless you definitely know what you are doing.
 adsdsd
