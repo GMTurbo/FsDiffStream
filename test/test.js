275f187fc7b111320eaaa63042856f2061e6bb51
Gabe Testa <gmturbo@me.com>
2014-10-24 14:57:21 -0600

    testing

--- test/test.js
+++ test/test.js
@@ -17,36 +17,50 @@ var fsDif = new Differ({
   ignoreDotFiles: true
 });
 
-var chunkSize = 1024 * 10;
+var chunkSize = 1024 * 4;
 
 var diffStream = new fsDiffStream({
   chunkSize: chunkSize
 });
 
 var outputFile,
-  offset,
+  offset, writing = false,
   rar = new randomAccessRemove();
 
 var orderFile = function(filename, blockCount, oldCount) {
 
   if (oldCount > blockCount) {
 
-    rar.remove(
-      filename,
-      blockCount * chunkSize,
-      (oldCount - blockCount) * chunkSize,
-      function(err) {
-      if (err)
-        console.error(err);
-    });
+    writing = true;
 
+    if (outputFile) {
+      outputFile.close(function() {
+        rar.remove(
+          filename,
+          blockCount * chunkSize, (oldCount - blockCount) * chunkSize,
+          function(err) {
+            if (err)
+              console.error(err);
+            writing = false;
+          });
+      });
+    } else {
+      rar.remove(
+        filename,
+        blockCount * chunkSize, (oldCount - blockCount) * chunkSize,
+        function(err) {
+          if (err)
+            console.error(err);
+          writing = false;
+        });
+    }
   }
 };
 
 diffStream.on('fileResize', function(err, data) {
   if (err) console.error(err);
   debugger;
-  orderFile(data.fileName, data.chunkCount, data.prevChunkCount);
+  //orderFile(path.basename(data.fileName), data.chunkCount, data.prevChunkCount);
   // outputFile.read(offset, chunk.data, function(err) {
   //   if(err) console.error(err);
   // });
@@ -54,7 +68,7 @@ diffStream.on('fileResize', function(err, data) {
 
 diffStream.on('chunkChanged', function(err, chunk) {
   if (err) console.error(err);
-  console.log('chunkChanged', chunk);
+  //console.log('chunkChanged', chunk);
   offset = chunk.id * chunk.targetChunkSize;
   outputFile.write(offset, chunk.data, function(err) {
     if (err) console.error(err);
@@ -71,8 +85,9 @@ diffStream.on('chunkRemoved', function(err, data) {
 });
 
 diffStream.on('uniqueChunk', function(err, chunk) {
-  if (err) console.error(err);
-  console.log('uniqueChunk', chunk);
+  if (err)
+    console.error(err);
+  //console.log('uniqueChunk', chunk);
   offset = chunk.id * chunk.targetChunkSize;
   outputFile.write(offset, chunk.data, function(err) {
     if (err) console.error(err);
@@ -99,15 +114,39 @@ fsDif.on('ready', function() {
   });
 
   fsDif.on('changed', function(err, data) {
-    //console.log('moved', data);
+    console.log('changed', data);
     if (err)
       process.exit();
 
-    if (outputFile)
-      outputFile.close();
+    var interval = setInterval(function(){
+      if(writing)
+        return;
+      clearInterval(interval);
+      handleChange(data);
+    }, 100);
 
-    outputFile = randomAccessFile(path.basename(data.fileName));
-    diffStream.compare(data.fileName);
   });
+
+  var handleChange = function(data){
+    if (outputFile) {
+      outputFile.close(function() {
+        outputFile = randomAccessFile(path.basename(data.fileName));
+        diffStream.compare(data.fileName, function(err, resized){
+          console.log('dif finished');
+          if(resized){
+            orderFile(path.basename(resized.fileName), resized.chunkCount, resized.prevChunkCount);
+          }
+        });
+      });
+    } else {
+      outputFile = randomAccessFile(path.basename(data.fileName));
+      diffStream.compare(data.fileName, function(err, resized){
+        console.log('dif finished');
+        if(resized){
+          orderFile(path.basename(resized.fileName), resized.chunkCount, resized.prevChunkCount);
+        }
+      });
+    }
+  }
 
 });
